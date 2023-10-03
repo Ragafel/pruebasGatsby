992adc470cf94894235df749b921a290fedc5fa3
Rafael <ramachado40@gmail.com>
2023-10-03 18:31:21 -0400

    Finished Gatsby Tutorial Part 5

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@
  */
 module.exports = {
   siteMetadata: {
-    title: "Aprendiendo Gatsby :D",
+    title: "Mi Super Cool Blog",
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
@@ -30,6 +30,7 @@ module.exports = {
         path: `${__dirname}/blog`,
       }
     },
+    "gatsby-plugin-mdx",
     `gatsby-transformer-sharp`,
     "gatsby-plugin-sharp",
     {
